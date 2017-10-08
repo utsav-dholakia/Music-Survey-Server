f728b7a5e7c39278f46510846731f34115c0ec20
Utsav Dholakia <uvd1992@gmail.com>
2017-10-08 16:34:29 -0500

    Create admin login authorization service and database initial connection methods

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const sqlite = require('sqlite3').verbose();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+var db;
+const dbFilePath = './db/UserSurvey.db';
+
+//Initialize database connection
+function initialize(){
+    this.db = new sqlite.Database(dbFilePath, function(err) {
+        if(err){
+            console.error('Error connecting to the database');
+        }
+        else{
+            console.log('connected to the database');
+        }
+    });
+
+}
+
+//Authorize login for admin
+router.post('/authorizeLogin', function(req, res) {
+    if(req.body.userName === "admin" && req.body.password === "password"){
+        res.sendStatus(200);
+    }
+    res.sendStatus(500);
 });
+
+
+// close the database connection
+function close(){
+
+    this.db.close(function(err){
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+    });
+}
+
+initialize();
+
 
 module.exports = router;
